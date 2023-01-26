c8598532d5734d55610ffd247bc853ddfcb40c61
Chris Santiago <chrisantiago217@gmail.com>
2023-01-26 09:56:12 -0500

    Sign up functionality

--- js/code.js
+++ js/code.js
@@ -42,7 +42,7 @@ function doLogin() {
 
         saveCookie()
 
-        window.location.href = 'color.html'
+        window.location.href = 'contact.html'
       }
     }
     xhr.send(jsonPayload)
@@ -53,8 +53,8 @@ function doLogin() {
 
 function doRegister() {
   userId = 0
-  // firstName = ''
-  // lastName = ''
+  firstName = ''
+  lastName = ''
 
   firstName = document.getElementById('firstName').value
   lastName = document.getElementById('lastName').value
